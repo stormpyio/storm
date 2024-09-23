f730295a13e6bf45ecc787e0fc76296d3910854b
Adib Grouz <contact@adib-grouz.com>
2024-09-23 23:49:25 +0200

    add basic example

---
+++ samples/basic-app/main.py
@@ -0,0 +1,27 @@
+from storm.common.decorators.http import Get
+from storm.common.decorators.module import Module
+from storm.common.decorators.controller import Controller
+from storm.core.application import StormApplication
+
+# Define Controller
+@Controller("/users")
+class UsersController:
+    def __init__(self):
+        print("UsersController initialized")
+    @Get("/")
+    def get_users(self):
+        return {"users": ["John", "Jane"]}
+
+# Define Module
+@Module(controllers=[UsersController], imports=[])
+class AppModule:
+    pass
+
+
+# Create the Storm Application and Run the Server
+if __name__ == "__main__":
+    # Initialize the application with AppModule
+    app = StormApplication(AppModule)
+
+    # Start the application
+    app.run()
